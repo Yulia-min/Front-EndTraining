8e592f34bf77e43c53830be5d4c73eaa25f14a02
Yulia <gallavich66@gmail.com>
2021-07-21 20:23:57 +0300

    firstTaskDependences

---
+++ firstTast(dependences)/index.js
@@ -0,0 +1,10 @@
+//https://learn.javascript.ru/testing-mocha
+//https://www.npmjs.com/package/is-not-thirteen
+
+// TASK
+// create app with one dependences and one devDependences
+
+module.exports = function isThirteen(thirteen) {
+    return thirteen;
+  }
+
